85d7964e25db713e03eb9dbf3527a7cbb7a6f665
kmdebug <kaissar.mouelhi@outlook.com>
2025-10-20 17:34:28 +0000

    fix(package): add homepage key with url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/react-browser-history-simulator/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
